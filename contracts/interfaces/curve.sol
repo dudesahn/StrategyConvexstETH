c4df539136f64d21f926b12a84621bc5fbacf5d6
dudesahn <dudesahn@gmail.com>
2021-05-22 17:28:47 -0400

    fix: run prettier

--- contracts/interfaces/curve.sol
+++ contracts/interfaces/curve.sol
@@ -19,9 +19,14 @@ interface Gauge {
 }
 
 interface ICurveFi {
-    function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount) external payable;
+    function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount)
+        external
+        payable;
 
-    function remove_liquidity_imbalance(uint256[2] calldata amounts, uint256 max_burn_amount) external;
+    function remove_liquidity_imbalance(
+        uint256[2] calldata amounts,
+        uint256 max_burn_amount
+    ) external;
 
     function remove_liquidity_one_coin(
         uint256 _token_amount,
@@ -29,9 +34,15 @@ interface ICurveFi {
         uint256 min_amount
     ) external;
 
-    function calc_token_amount(uint256[2] calldata amounts, bool is_deposit) external view returns (uint256);
+    function calc_token_amount(uint256[2] calldata amounts, bool is_deposit)
+        external
+        view
+        returns (uint256);
 
-    function calc_withdraw_one_coin(uint256 amount, int128 i) external view returns (uint256);
+    function calc_withdraw_one_coin(uint256 amount, int128 i)
+        external
+        view
+        returns (uint256);
 }
 
 interface ICrvV3 is IERC20 {
